aa6b832a0bd9f255e7e1ce6415fcaa0de8def716
Prahlad Inala <prahladinala@users.noreply.github.com>
2021-04-01 17:30:54 +0530

    Vue Edited And Added Bootstrap CDN to index.html

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,12 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Prahladinala.com"/>
-  </div>
+  <h1>Hello From Vue</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+export default{}
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style >
+
 </style>
